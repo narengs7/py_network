be749ded70e121a8f023972415a68dd63980e8f1
narengs7 <narengs7@gmail.com>
2019-09-16 18:09:18 +0530

    take client

--- client.py
+++ client.py
@@ -3,10 +3,16 @@ import socket
 
 # clinet program using python
 s = socket.socket()
-ip = '172.16.50.192'
-port = 1632
+ip = '192.168.43.149'
+port = 1633
 s.connect((ip,port))
 print("Successfully connected to ",ip)
-print(" Data : ",s.recv(1024))
+with open('sum_client.py','r') as fd:
+	str = fd.read()
+	s.sendall(b'sum_client.py')
+	print("Sent filename")
+	s.sendall(bytes(str,'utf-8'))
+	
+print(s.recv(1024))
 s.close()
 
