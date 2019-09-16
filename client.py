74ba49aae5a74320e61bfc721be851f7ce2556f9
narengs7 <narengs7@gmail.com>
2019-09-16 19:06:21 +0530

    Client code complete

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 #! /usr/bin/env python3
 import socket
+import sys
 
+#read file
+fileName = sys.argv[1]
 # clinet program using python
 s = socket.socket()
 ip = '192.168.43.149'
-port = 1633
+port = 1632
 s.connect((ip,port))
 print("Successfully connected to ",ip)
-with open('sum_client.py','r') as fd:
+with open(fileName,'r') as fd:
 	str = fd.read()
-	s.sendall(b'sum_client.py')
-	print("Sent filename")
+	s.sendall(bytes(fileName,'utf-8'))
+	print("Last Modified time",s.recv(1024))
 	s.sendall(bytes(str,'utf-8'))
 	
-print(s.recv(1024))
 s.close()
 
