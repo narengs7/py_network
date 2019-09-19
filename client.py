1f1b3b41341b5a510a76352296d1ac9e304c8cd9
narengs7 <narengs7@gmail.com>
2019-09-19 20:23:54 +0530

    Client pending document

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import socket
 import sys
+import time
 
 #read file
 fileName = sys.argv[1]
@@ -11,10 +12,22 @@ port = 1632
 s.connect((ip,port))
 print("Successfully connected to ",ip)
 with open(fileName,'r') as fd:
-	str = fd.read()
 	s.sendall(bytes(fileName,'utf-8'))
-	print("Last Modified time",s.recv(1024))
-	s.sendall(bytes(str,'utf-8'))
+	time.sleep(0.5)
+	for line in fd:
+		s.send(bytes(line,'utf-8'))
+
+time.sleep(0.5)	
+s.sendall(b' ')
+val = s.recv(1024)
+while True:
+	print(val)
+	val = s.recv(1024)
+	if val==b'\n':
+		break
 	
+
+print("Done Reading file")
+print("Last Modified time",s.recv(1024))
 s.close()
 
